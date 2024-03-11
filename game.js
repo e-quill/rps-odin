ab90d507650281f885beb19dd351161063b46fe5
e-quill <e-quill@users.noreply.github.com>
2024-03-11 13:54:05 +0530

    Add playGame()

--- game.js
+++ game.js
@@ -16,23 +16,50 @@ function getComputerChoice(){
 function playRound(playerSelection, computerSelection){
 
   if (playerSelection.toLowerCase() == "rock" && computerSelection == "Scissors") {
+    playerScore++
     return "You Win! Rock beats Scissors!!";
   } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Rock"){
+    playerScore++
     return "You Win! Paper beats Rock!!";
   } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Paper"){
+    playerScore++
     return "You Win! Scissors beats Paper!!";
   } else if (playerSelection.toLowerCase() == "rock" && computerSelection == "Paper") {
+    computerScore++
     return "You Lose! Paper beats Rock!!";
   } else if (playerSelection.toLowerCase() == "paper" && computerSelection == "Scissors"){
+    computerScore++
     return "You Lose! Scissors beats Paper!!";
   } else if (playerSelection.toLowerCase() == "scissors" && computerSelection == "Rock"){
+    computerScore++
     return "You Lose! Rock beats Scissors!!";
   }else {
-    return "Its a Draw!"
+    return `Both players chose ${computerSelection}, its a Draw!`
   }   
 
 }
 
-// const playerSelection = "PAPER";
-// const computerSelection = getComputerChoice();
-// console.log(playRound(playerSelection, computerSelection));
+function playGame(){
+  round = 1;
+  playerScore = 0;
+  computerScore = 0;
+
+  for (i = 0; i < 5; i++, round++){
+    choice = prompt("Rock, Paper or Scissors?")
+    console.log(`Round ${round}`)
+    console.log(playRound(choice, getComputerChoice()))
+  }
+
+  console.log("")
+
+  console.log(`The final score is ${playerScore}:${computerScore}`)
+  if (playerScore > computerScore){
+    console.log("You have won!")
+  } else if (playerScore < computerScore) {
+    console.log("You have lost, better luck next time.")
+  } else {
+    console.log("Both of you were equally matched, its a draw.")
+  }
+}
+
+playGame()
